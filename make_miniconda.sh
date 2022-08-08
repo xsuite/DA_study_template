f10c2b470e547132b722b24aea81c385be8f2552
sterbini <guido.sterbini@cern.ch>
2022-08-08 15:13:20 +0200

    Update make_miniconda.sh

--- make_miniconda.sh
+++ make_miniconda.sh
@@ -8,6 +8,7 @@ git clone git@github.com:lhcopt/lhcmask.git
 python -m pip install -e lhcmask
 git clone git@github.com:lhcopt/lhcerrors.git
 git clone git@github.com:lhcopt/lhctoolkit.git
+git clone git@github.com:lhcopt/hllhc15.git
 cd master_study/master_jobs/001_machine_model/
 python 001_copy_from_pymask_examples.py
 cd ../../../
