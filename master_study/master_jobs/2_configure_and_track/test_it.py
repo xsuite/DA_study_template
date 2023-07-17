1bd9a63010ae3e562754abadee4d0ca44429e045
Guido Sterbini <guido.sterbini@cern.ch>
2023-07-17 16:44:21 +0200

    minors

---
+++ master_study/master_jobs/2_configure_and_track/test_it.py
@@ -0,0 +1,37 @@
+import xtrack as xt
+import numpy as np
+import pandas as pd
+
+collider = xt.Multiline.from_json('collider.json')
+
+collider.build_trackers()
+
+
+twiss_b1 = collider['lhcb1'].twiss()
+twiss_b2 = collider['lhcb2'].twiss().reverse()
+
+survey_b1 = collider['lhcb1'].survey()
+survey_b2 = collider['lhcb2'].survey().reverse()
+ 
+for my_ip in ['on_alice_normalized','on_lhcb_normalized']:
+    print(f'*****************\nValues for {my_ip} (polarity):')
+    print(collider.vars[my_ip]._value)
+    print(f'*****************\n')
+
+for my_ip in [1,2,5,8]:
+    print(f'*****************\nValues for IP{my_ip}:')
+    my_df = []
+    for my_table, my_beam in zip([twiss_b1, twiss_b2],[1,2]):
+        my_df.append(my_table[
+            ['x', 'y', 'px', 'py', 'betx', 'bety', 'alfx', 'alfy'],
+            f'ip{my_ip}'].to_pandas())
+        my_df[-1].index = [f'B{my_beam}']
+    print(pd.concat(my_df, axis=0).transpose())
+    print(f'*****************\n')
+
+
+mydf = my_table[['x', 'y', 'px', 'py', 'betx', 'bety', 'alfx', 'alfy'],
+            f'ip{my_ip}'].to_pandas()
+mydf.name = 'B1'
+mydf
+
