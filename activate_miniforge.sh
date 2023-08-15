3213b0fcbb3bb9fdbba0eb6e5b4a1ff5e2a3cb82
Colas Droin <colasdroin@gmail.com>
2023-08-15 17:04:52 +0200

    Cleaned activate_miniforge

--- activate_miniforge.sh
+++ activate_miniforge.sh
@@ -1,3 +1,6 @@
+# Get current directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Get if docker miniconda exists
 FILE=/usr/local/DA_study/miniforge_docker/bin/activate
 
@@ -6,7 +9,7 @@ if [[ -f "$FILE" ]];
 
 # source the make_miniforge.sh script, concatenating the path to the script directory
 then
-    source /usr/local/DA_study/miniforge_docker/bin/activate
+    source $FILE
 # Only activate environment 
 else
     source $SCRIPT_DIR/miniforge/bin/activate
