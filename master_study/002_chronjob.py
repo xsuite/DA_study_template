404adaffb971fb8b2650aaaee3d82da743965d57
Colas Droin <colasdroin@gmail.com>
2023-08-25 16:40:42 +0200

    Update name studies

--- master_study/002_chronjob.py
+++ master_study/002_chronjob.py
@@ -384,7 +384,7 @@ def submit_jobs(study_name, print_uncompleted_jobs=False):
 # Load the tree from a yaml and submit the jobs that haven't been completed yet
 if __name__ == "__main__":
     # Define study
-    study_name = "example_HL_tunescan"
+    study_name = "example_tunescan"
 
     # Submit jobs
     submit_jobs(study_name)
