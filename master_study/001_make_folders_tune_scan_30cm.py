ec4714401dd6aeb5d9c10fb872138705a081a58f
Colas Droin <colasdroin@gmail.com>
2024-02-27 11:37:25 +0100

    Update studies with correct number of turns

--- master_study/001_make_folders_tune_scan_30cm.py
+++ master_study/001_make_folders_tune_scan_30cm.py
@@ -265,7 +265,7 @@ d_config_collider["config_beambeam"] = d_config_beambeam
 d_config_simulation = {}
 
 # Number of turns to track
-d_config_simulation["n_turns"] = 200
+d_config_simulation["n_turns"] = 1000000
 
 # Initial off-momentum
 d_config_simulation["delta_max"] = 27.0e-5
