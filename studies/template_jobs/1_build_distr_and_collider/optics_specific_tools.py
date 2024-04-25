71ad175df8218af2fabc785e6e4f5fadef07f845
Colas Droin <colasdroin@gmail.com>
2024-04-25 16:46:34 +0200

    Adapted code for ions

--- studies/template_jobs/1_build_distr_and_collider/optics_specific_tools.py
+++ studies/template_jobs/1_build_distr_and_collider/optics_specific_tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import xmask as xm
 
 
 def check_madx_lattices(mad):
@@ -7,22 +8,29 @@ def check_madx_lattices(mad):
     assert mad.globals["qpxb1"] == mad.globals["qpxb2"]
     assert mad.globals["qpyb1"] == mad.globals["qpyb2"]
 
-    assert np.isclose(mad.table.summ.q1, mad.globals["qxb1"], atol=1e-05)
-    assert np.isclose(mad.table.summ.q2, mad.globals["qyb1"], atol=1e-05)
+    try:
+        assert np.isclose(mad.table.summ.q1, mad.globals["qxb1"], atol=1e-02)
+        assert np.isclose(mad.table.summ.q2, mad.globals["qyb1"], atol=1e-02)
+        assert np.isclose(mad.table.summ.dq1, mad.globals["qpxb1"], atol=5e-01)
+        assert np.isclose(mad.table.summ.dq2, mad.globals["qpyb1"], atol=5e-01)
+    except AssertionError:
+        print("Warning: some of the Qx, Qy, DQx, DQy values are not close to the expected ones")
+
+    df = mad.table.twiss.dframe()
+    for my_ip in [1, 2, 5, 8]:
+        # assert np.isclose(df.loc[f"ip{my_ip}"].betx, mad.globals[f"betx_IP{my_ip}"], rtol=1e-02)
+        # assert np.isclose(df.loc[f"ip{my_ip}"].bety, mad.globals[f"bety_IP{my_ip}"], rtol=1e-02)
+        assert np.isclose(df.loc[f"ip{my_ip}"].betx, mad.globals[f"betxIP{my_ip}b1"], rtol=1e-02)
+        assert np.isclose(df.loc[f"ip{my_ip}"].bety, mad.globals[f"betyIP{my_ip}b1"], rtol=1e-02)
+
+    mad.twiss()
+    df = mad.table.twiss.dframe()
 
     try:
-        assert np.isclose(mad.table.summ.dq1, mad.globals["qpxb1"], atol=1e-01)
-        assert np.isclose(mad.table.summ.dq2, mad.globals["qpyb1"], atol=1e-01)
-
-        df = mad.table.twiss.dframe()
-        for my_ip in [1, 2, 5, 8]:
-            assert np.isclose(df.loc[f"ip{my_ip}"].betx, mad.globals[f"betx_IP{my_ip}"], rtol=1e-03)
-            assert np.isclose(df.loc[f"ip{my_ip}"].bety, mad.globals[f"bety_IP{my_ip}"], rtol=1e-03)
-
-        assert df["x"].std() < 1e-8
-        assert df["y"].std() < 1e-8
+        assert df["x"].std() < 1e-6
+        assert df["y"].std() < 1e-6
     except AssertionError:
-        print("WARNING: Some sanity checks have failed during the madx lattice check")
+        print("Warning: the standard deviation of x and y are not close to zero")
 
 
 def check_xsuite_lattices(my_line):
@@ -34,103 +42,55 @@ def check_xsuite_lattices(my_line):
     print(tw.qx, tw.qy)
 
 
-def build_sequence(mad, mylhcbeam, ignore_cycling=False):
+def build_sequence(mad, mylhcbeam, beam_config, ignore_cycling=False, slice_factor=8):
     # Select beam
     mad.input(f"mylhcbeam = {mylhcbeam}")
+    mad.input("option, -echo,warn, -info;")
 
-    mad.input(f"""
-      ! Get the toolkit
-      call,file=
-        "acc-models-lhc/toolkit/macro.madx";
-      """)
+    # optics dependent macros (for splitting)
+    mad.call("acc-models-lhc/runII/2018/toolkit/macro.madx")
 
-    mad.input(f"""
-      ! Build sequence
-      option, -echo,-warn,-info;
-      if (mylhcbeam==4){{
-        call,file="acc-models-lhc/lhc_acc-models-lhc_b4.seq";
-      }} else {{
-        call,file="acc-models-lhc/lhc_acc-models-lhc.seq";
-      }};
-      option, -echo, warn,-info;
-      """)
+    assert mylhcbeam in [1, 2, 4], "Invalid mylhcbeam (it should be in [1, 2, 4])"
 
-    # Redefine macro for myslice
-    my_slice(mad, slice_factor=4)
+    if mylhcbeam in [1, 2]:
+        mad.call("acc-models-lhc/runII/2018/lhc_as-built.seq")
+    else:
+        mad.call("acc-models-lhc/runII/2018/lhcb4_as-built.seq")
 
-    # Slice nominal sequence
-    mad.input("exec, myslice;")
+    # New IR7 MQW layout and cabling
+    mad.call("acc-models-lhc/runIII/RunIII_dev/IR7-Run3seqedit.madx")
 
-    mad.input(f"""
-    nrj=6800;
-    beam,particle=proton,sequence=lhcb1,energy=nrj,npart=1.15E11,sige=4.5e-4;
-    beam,particle=proton,sequence=lhcb2,energy=nrj,bv = -1,npart=1.15E11,sige=4.5e-4;
-    """)
+    # Makethin part
+    if slice_factor > 0:
+        # the variable in the macro is slice_factor
+        mad.input(f"slicefactor={slice_factor};")
+        mad.call("acc-models-lhc/runII/2018/toolkit/myslice.madx")
+        for my_sequence in list(mad.sequence):
+            xm.attach_beam_to_sequence(
+                mad.sequence[my_sequence], int(my_sequence[-1]), beam_config[my_sequence]
+            )
+        # mad.beam()
+        for my_sequence in ["lhcb1", "lhcb2"]:
+            if my_sequence in list(mad.sequence):
+                mad.input(
+                    f"use, sequence={my_sequence}; makethin,"
+                    f"sequence={my_sequence}, style=teapot, makedipedge=true;"
+                )
+    else:
+        print("WARNING: The sequences are not thin!")
 
+    # Cycling w.r.t. to IP3 (mandatory to find closed orbit in collision in the presence of errors)
     if not ignore_cycling:
-        mad.input("""
-        !Cycling w.r.t. to IP3 (mandatory to find closed orbit in collision in the presence of errors)
-        if (mylhcbeam<3){
-        seqedit, sequence=lhcb1; flatten; cycle, start=IP3; flatten; endedit;
-        };
-        seqedit, sequence=lhcb2; flatten; cycle, start=IP3; flatten; endedit;
-        """)
-
-    mad.input("""
-        ! Set twiss formats for MAD-X parts (macro from opt. toolkit)
-        exec, twiss_opt;
-        """)
+        for my_sequence in ["lhcb1", "lhcb2"]:
+            if my_sequence in list(mad.sequence):
+                mad.input(
+                    f"seqedit, sequence={my_sequence}; flatten;"
+                    "cycle, start=IP3; flatten; endedit;"
+                )
 
 
 def apply_optics(mad, optics_file):
     mad.call(optics_file)
-    # A knob redefinition
-    mad.input("on_alice := on_alice_normalized * 7000./nrj;")
-    mad.input("on_lhcb := on_lhcb_normalized * 7000./nrj;")
-
-
-def my_slice(mad, slice_factor=2):
-    mad.input(f"slicefactor = {slice_factor};")
-    mad.input("""
-        myslice: macro = {
-        if (MBX.4L2->l>0) {
-          select, flag=makethin, clear;
-          select, flag=makethin, class=mb, slice=2;
-          select, flag=makethin, class=mq, slice=2 * slicefactor;
-          select, flag=makethin, class=mqxa,  slice=16 * slicefactor;  !old triplet
-          select, flag=makethin, class=mqxb,  slice=16 * slicefactor;  !old triplet
-          select, flag=makethin, class=mqxc,  slice=16 * slicefactor;  !new mqxa (q1,q3)
-          select, flag=makethin, class=mqxd,  slice=16 * slicefactor;  !new mqxb (q2a,q2b)
-          select, flag=makethin, class=mqxfa, slice=16 * slicefactor;  !new (q1,q3 v1.1)
-          select, flag=makethin, class=mqxfb, slice=16 * slicefactor;  !new (q2a,q2b v1.1)
-          select, flag=makethin, class=mbxa,  slice=4;   !new d1
-          select, flag=makethin, class=mbxf,  slice=4;   !new d1 (v1.1)
-          select, flag=makethin, class=mbrd,  slice=4;   !new d2 (if needed)
-          select, flag=makethin, class=mqyy,  slice=4 * slicefactor;;   !new q4
-          select, flag=makethin, class=mqyl,  slice=4 * slicefactor;;   !new q5
-          select, flag=makethin, class=mbh,   slice=4;   !11T dipoles
-          select, flag=makethin, pattern=mbx\.,    slice=4;
-          select, flag=makethin, pattern=mbrb\.,   slice=4;
-          select, flag=makethin, pattern=mbrc\.,   slice=4;
-          select, flag=makethin, pattern=mbrs\.,   slice=4;
-          select, flag=makethin, pattern=mbh\.,    slice=4;
-          select, flag=makethin, pattern=mqwa\.,   slice=4 * slicefactor;
-          select, flag=makethin, pattern=mqwb\.,   slice=4 * slicefactor;
-          select, flag=makethin, pattern=mqy\.,    slice=4 * slicefactor;
-          select, flag=makethin, pattern=mqm\.,    slice=4 * slicefactor;
-          select, flag=makethin, pattern=mqmc\.,   slice=4 * slicefactor;
-          select, flag=makethin, pattern=mqml\.,   slice=4 * slicefactor;
-          select, flag=makethin, pattern=mqtlh\.,  slice=2 * slicefactor;
-          select, flag=makethin, pattern=mqtli\.,  slice=2 * slicefactor;
-          select, flag=makethin, pattern=mqt\.  ,  slice=2 * slicefactor;
-          !thin lens
-          option rbarc=false; beam;
-          use,sequence=lhcb1; makethin,sequence=lhcb1,makedipedge=true,style=teapot;
-          use,sequence=lhcb2; makethin,sequence=lhcb2,makedipedge=true,style=teapot;
-          option rbarc=true;
-        } else {
-          print, text="Sequence is already thin";
-        };
-          is_thin=1;
-        };
-    """)
+    mad.call("ir7_strengths.madx")
+    mad.input("on_alice := on_alice_normalized * 7000. / nrj;")
+    mad.input("on_lhcb := on_lhcb_normalized * 7000. / nrj;")
