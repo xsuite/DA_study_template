faee6b4944389427efa2a741d66ddc9112dd43e9
Colas <colasdroin@gmail.com>
2024-06-24 15:52:34 +0200

    Few fixs for better submission method

--- studies/template_jobs/2_configure_and_track/misc.py
+++ studies/template_jobs/2_configure_and_track/misc.py
@@ -480,7 +480,6 @@ def generate_orbit_correction_setup():
         },
     }
 
-
 def compute_PU(luminosity, num_colliding_bunches, T_rev0, cross_section=81e-27):
     return luminosity / num_colliding_bunches * cross_section * T_rev0
 
